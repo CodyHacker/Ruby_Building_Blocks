be62354ee3e990c1ca02d64281db4656f7b3bb95
Cody Hacker <jbalsamo@cox.net>
2015-06-05 18:57:44 -0700

    DRY Clean again

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,21 +6,13 @@ Alpha_Lower = ("a".."z").to_a
 def caesar_cipher(the_string, cipher_offset)
 	cipher_string = ""
 	the_string.each_char do |char|
-		if char =~ /[A-Za-z]/
-			cipher_string << cipherize(char, cipher_offset)
-		else
-			cipher_string << char
-		end
+		char =~ /[A-Za-z]/ ? cipher_string << cipherize(char, cipher_offset) : cipher_string << char
 	end
 	cipher_string
 end
 
 def cipherize(char, cipher_offset)
-	if char.downcase == char
-		the_Alpha = Alpha_Lower
-	else
-		the_Alpha = Alpha_Upper
-	end
+	the_Alpha = char.downcase == char ? Alpha_Lower : Alpha_Upper
 	index_number = the_Alpha.index(char) + cipher_offset
 	index_number -= the_Alpha.size if index_number > 25
 	return_char = the_Alpha[index_number]
