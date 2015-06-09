2aa79c4483b347a33ec5a022790b3d34af1bac29
Cody Hacker <jbalsamo@cox.net>
2015-06-08 17:09:29 -0700

    fixed for edge cases

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,6 +4,11 @@ def stock_picker(daily_closing_prices)
 
 	buy_sell_pairs = daily_closing_prices.combination(2).to_a.delete_if { |pair| pair[0] >= pair[1] }
 
+	if buy_sell_pairs.size < 2 
+		puts "No buy/sell pairs"
+		return
+	end
+
 	buy_sell_spreads = buy_sell_pairs.collect { |pair| pair[1] - pair[0] }
 
 	biggest_buy_sell_pair = buy_sell_pairs[buy_sell_spreads.find_index(buy_sell_spreads.max)]
@@ -11,4 +16,5 @@ def stock_picker(daily_closing_prices)
 	pair_of_days << daily_closing_prices.find_index(biggest_buy_sell_pair[0]) << daily_closing_prices.find_index(biggest_buy_sell_pair[1])
 end
 
+ p stock_picker([17,1]) #thank you, 
  p stock_picker([17,3,6,9,15,8,6,1,10])
